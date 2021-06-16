a87a987b9a66d158cbd853f62cbf2cfd8a5eeb04
kotkotei <kotkotei@yandex.ru>
2021-06-16 11:47:55 +0700

    2 level 3ada4a

---
+++ Program 1 3ada4a.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = n / 100;
+            int c = n % 10;
+            int b = n % 100 / 10;
+            if (a == b || b == c)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+            Console.ReadKey();
+
+
+            }
+        }
+    }
+
